a3695ba8748d70ec81cc3e927c2d2f9a9722db20
Christopher Gerber <christopher.gerber@coinbase.com>
2024-11-27 01:52:32 -0800

    adding todo for test utils mock

--- cdp-agentkit-core/src/tests/utils/mock.ts
+++ cdp-agentkit-core/src/tests/utils/mock.ts
@@ -1,3 +1,4 @@
 export const mockFn = (...args) => jest.fn(...args) as any;
 export const mockReturnRejectedValue = data => jest.fn().mockRejectedValue(data);
+// TODO: remove the enclosing obj
 export const mockReturnValue = data => jest.fn().mockResolvedValue({ data });
