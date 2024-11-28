3b479cab5d87ad64af2884a1b34432ac90fd48ed
Christopher Gerber <christopher.gerber@coinbase.com>
2024-11-27 17:06:59 -0800

    removing dead code from contract invocation mock

--- cdp-agentkit-core/src/tests/factories/contract_invocation.ts
+++ cdp-agentkit-core/src/tests/factories/contract_invocation.ts
@@ -2,29 +2,7 @@ import { SmartContract } from "@coinbase/coinbase-sdk";
 import { TransactionData } from "./smart_contract";
 import { mockFn } from "../utils/mock";
 
-export interface ContractInvocation {
-  network_id: string;
-  wallet_id: string;
-  address_id: string;
-  contract_invocation_id: string;
-  contract_address: string;
-  method: string;
-  args: string;
-  abi?: string;
-  amount: string;
-  transaction: TransactionData;
-}
-
-// export const newContractInvocationFactory = (data: ContractInvocation) => {
 export const newContractInvocationFactory = () => {
-  // const getContractInvocationFn = mockFn(_ => {
-  //   return { data: data };
-  // });
-
-  // const createContractInvocationFn = mockFn(request => {
-  //   return { data: data };
-  // });
-
   return {
     getContractInvocation: jest.fn(),
     listContractInvocations: jest.fn(),
