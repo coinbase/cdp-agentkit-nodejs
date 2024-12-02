b77b30028593c3ec22b5fa8fa54b8188155c94ad
Christopher Gerber <christopher.gerber@coinbase.com>
2024-12-02 11:13:38 -0800

    first pass implementing register basename test

--- cdp-agentkit-core/src/tests/actions/register_basename_test.ts
+++ cdp-agentkit-core/src/tests/actions/register_basename_test.ts
@@ -1,9 +1,20 @@
-import { Coinbase, Wallet } from "@coinbase/coinbase-sdk";
+import {
+  Coinbase,
+  ContractInvocation,
+  CreateContractInvocationOptions,
+  Wallet,
+} from "@coinbase/coinbase-sdk";
 
-import { RegisterBasenameInput } from "../../actions/cdp/register_basename";
+import { registerBasename, RegisterBasenameInput } from "../../actions/cdp/register_basename";
 
+import { newContractInvocationFactory } from "../factories/contract_invocation";
 import { newWalletFactory } from "../factories/wallet";
 import { newWalletAddressFactory } from "../factories/wallet_address";
+import {
+  generateContractInvocationData,
+  generateContractInvocationFromData,
+} from "../utils/contract_invocation";
+import { mockReturnValue, mockReturnRejectedValue } from "../utils/mock";
 import { generateWalletData } from "../utils/wallet";
 
 const MOCK_AMMOUNT = 0.123;
@@ -31,6 +42,8 @@ describe("Register Basename Input", () => {
 });
 
 describe("Register Basename Action", () => {
+  let contractInvocation: ContractInvocation;
+  let contractInvocationOptions: CreateContractInvocationOptions;
   let wallet: Wallet;
 
   beforeAll(async () => {
@@ -44,9 +57,113 @@ describe("Register Basename Action", () => {
     wallet = await Wallet.create();
   });
 
-  beforeEach(() => {});
+  it("should fail with an error", async () => {
+    const error = new Error("Failed to register basename");
 
-  it("should successfully register basename", async () => {});
+    (await wallet.getDefaultAddress()).invokeContract = mockReturnRejectedValue(error)
 
-  it("should fail with an error", async () => {});
+    const response = await registerBasename(wallet, {...MOCK_OPTIONS});
+    const expected = `Error registering basename: ${error.message}`;
+
+    expect(response).toEqual(expected);
+  });
+});
+// TODO: additional network testing?
+
+describe("Register Basename Action on base.eth", () => {
+  let contractInvocation: ContractInvocation;
+  let contractInvocationOptions: CreateContractInvocationOptions;
+  let wallet: Wallet;
+
+  beforeAll(async () => {
+    const walletDataOptions = {
+      networkId: Coinbase.networks.BaseMainnet
+    }
+
+    const walletData = generateWalletData(walletDataOptions);
+    const walletAddresses = [walletData.address];
+
+    Coinbase.apiClients.address = newWalletAddressFactory(walletAddresses);
+    Coinbase.apiClients.wallet = newWalletFactory(walletData);
+    Coinbase.useServerSigner = false;
+
+    wallet = await Wallet.create();
+  });
+
+  beforeEach(async () => {
+    (await wallet.getDefaultAddress()).invokeContract = jest
+      .fn()
+      .mockImplementation(async (params) => {
+        const contractInvocationData = generateContractInvocationData(wallet, params);
+
+        contractInvocation = generateContractInvocationFromData(params);
+        contractInvocationOptions = params;
+
+        Coinbase.apiClients.contractInvocation = newContractInvocationFactory();
+        Coinbase.apiClients.contractInvocation.getContractInvocation =
+          mockReturnValue(contractInvocationData);
+
+        return generateContractInvocationFromData(contractInvocationData);
+      });
+  });
+
+  it("should successfully respond", async () => {
+    const response = await registerBasename(wallet, {...MOCK_OPTIONS});
+    const expected = `Successfully registered basename ${MOCK_BASENAME}.base.eth for address ${(await wallet.getDefaultAddress()).getId()}`;
+
+    expect((await wallet.getDefaultAddress()).invokeContract).toHaveBeenCalledTimes(1);
+    expect((await wallet.getDefaultAddress()).invokeContract).toHaveBeenCalledWith(
+      contractInvocationOptions,
+    );
+    expect(response).toBe(expected);
+  });
+});
+
+describe("Register Basename Action on basetest.eth", () => {
+  let contractInvocation: ContractInvocation;
+  let contractInvocationOptions: CreateContractInvocationOptions;
+  let wallet: Wallet;
+
+  beforeAll(async () => {
+    const walletDataOptions = {
+      networkId: "basetest.eth, or any value not base-mainnet",
+    }
+
+    const walletData = generateWalletData(walletDataOptions);
+    const walletAddresses = [walletData.address];
+
+    Coinbase.apiClients.address = newWalletAddressFactory(walletAddresses);
+    Coinbase.apiClients.wallet = newWalletFactory(walletData);
+    Coinbase.useServerSigner = false;
+
+    wallet = await Wallet.create();
+  });
+
+  beforeEach(async () => {
+    (await wallet.getDefaultAddress()).invokeContract = jest
+      .fn()
+      .mockImplementation(async (params) => {
+        const contractInvocationData = generateContractInvocationData(wallet, params);
+
+        contractInvocation = generateContractInvocationFromData(params);
+        contractInvocationOptions = params;
+
+        Coinbase.apiClients.contractInvocation = newContractInvocationFactory();
+        Coinbase.apiClients.contractInvocation.getContractInvocation =
+          mockReturnValue(contractInvocationData);
+
+        return generateContractInvocationFromData(contractInvocationData);
+      });
+  });
+
+  it("should successfully respond on testnet", async () => {
+    const response = await registerBasename(wallet, {...MOCK_OPTIONS});
+    const expected = `Successfully registered basename ${MOCK_BASENAME}.basetest.eth for address ${(await wallet.getDefaultAddress()).getId()}`;
+
+    expect((await wallet.getDefaultAddress()).invokeContract).toHaveBeenCalledTimes(1);
+    expect((await wallet.getDefaultAddress()).invokeContract).toHaveBeenCalledWith(
+      contractInvocationOptions,
+    );
+    expect(response).toBe(expected);
+  });
 });
--- cdp-agentkit-core/src/tests/utils/wallet.ts
+++ cdp-agentkit-core/src/tests/utils/wallet.ts
@@ -1,25 +1,39 @@
-import { Coinbase } from "@coinbase/coinbase-sdk";
+import { Coinbase, WalletCreateOptions } from "@coinbase/coinbase-sdk";
 import { ethers } from "ethers";
 
 import { WalletData } from "../factories/wallet";
 import { generateWalletAddressDataWithSeed } from "./wallet_address";
 import { generateSeed } from "./seed";
 
-export const generateWalletData = (): WalletData => {
-  return generateWalletDataWithSeed(generateSeed());
-};
+export interface GenerateWalletDataOptions extends WalletCreateOptions {
+  seed?:string
+}
 
-export const generateWalletDataWithSeed = (
-  seed: string,
-  network: string = Coinbase.networks.BaseSepolia,
-): WalletData => {
+export const DefaultGenerateWalletDataOptions = {
+  networkId: Coinbase.networks.BaseSepolia,
+  seed: "",
+}
+
+// export const generateWalletData = (): WalletData => {
+//   return generateWalletDataWithOptions();
+// };
+
+export const generateWalletData = (options: GenerateWalletDataOptions = DefaultGenerateWalletDataOptions) => {
+  if (!options.networkId) {
+    throw new Error("wallet network is required");
+  }
+
+  if (!options.seed) {
+    options.seed = generateSeed()
+  }
+
   const walletId = crypto.randomUUID();
-  const walletAddress = generateWalletAddressDataWithSeed(walletId, seed);
+  const walletAddress = generateWalletAddressDataWithSeed(walletId, options.seed);
 
   return {
     id: walletId,
     address: walletAddress,
-    networkId: network,
-    privateKey: "",
+    networkId: options.networkId,
+    privateKey: options.seed,
   };
 };
