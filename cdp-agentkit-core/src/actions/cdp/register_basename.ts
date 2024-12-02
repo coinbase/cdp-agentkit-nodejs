ba2684261abdf9ab14d14b8bf65e6b2c8f8b1c8c
Christopher Gerber <christopher.gerber@coinbase.com>
2024-12-01 22:02:34 -0800

    exporting register basename

--- cdp-agentkit-core/src/actions/cdp/register_basename.ts
+++ cdp-agentkit-core/src/actions/cdp/register_basename.ts
@@ -95,7 +95,7 @@ const REGISTRAR_ABI = [
 /**
  * Input schema for registering a Basename.
  */
-const RegisterBasenameInput = z
+export const RegisterBasenameInput = z
   .object({
     basename: z.string().describe("The Basename to assign to the agent"),
     amount: z.string().default("0.002").describe("The amount of ETH to pay for registration"),
