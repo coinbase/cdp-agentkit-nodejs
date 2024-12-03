ac5988c505dd6f32007a91119b89ed0f8e603d6e
Derek Ricciuto <derek.ricciuto@coinbase.com>
2024-12-03 15:11:30 -0500

    Adjusting jest configs, adding back automatic GH action for unit tests

--- cdp-agentkit-core/jest.config.js
+++ cdp-agentkit-core/jest.config.js
@@ -2,4 +2,19 @@ import baseConfig from "../jest.config.base.js";
 
 export default {
   ...baseConfig,
+  coveragePathIgnorePatterns: ["node_modules", "dist", "docs", "index.ts"],
+  coverageThreshold: {
+    "./src/actions/cdp/*": {
+      branches: 50,
+      functions: 50,
+      statements: 50,
+      lines: 50,
+    },
+    "./src/actions/cdp/defi/wow/actions/*": {
+      branches: 50,
+      functions: 50,
+      statements: 50,
+      lines: 50,
+    },
+  },
 };
--- cdp-langchain/jest.config.js
+++ cdp-langchain/jest.config.js
@@ -2,4 +2,13 @@ import baseConfig from "../jest.config.base.js";
 
 export default {
   ...baseConfig,
+  coveragePathIgnorePatterns: ["node_modules", "dist", "docs", "index.ts"],
+  coverageThreshold: {
+    "./src/**": {
+      branches: 50,
+      functions: 50,
+      statements: 50,
+      lines: 50,
+    },
+  },
 };
