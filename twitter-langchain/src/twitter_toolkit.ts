6e3689264d3bde42cff2b690039125fd745bc8d9
Christopher Gerber <christopher.gerber@coinbase.com>
2024-12-05 16:22:08 -0800

    chatbot functional

--- twitter-langchain/src/twitter_toolkit.ts
+++ twitter-langchain/src/twitter_toolkit.ts
@@ -50,6 +50,5 @@ export class TwitterToolkit extends Toolkit {
     const actions = TWITTER_ACTIONS;
     const tools = actions.map(action => new TwitterTool(action, agentkit));
     this.tools = tools;
-    this.tools = [];
   }
 }
